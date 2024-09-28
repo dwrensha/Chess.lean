5b17ef8572da4f35fe5417d730d7cba636be9f45
David Renshaw <dwrenshaw@gmail.com>
2024-09-28 08:45:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chess in Lean 4
+
+Originally developed as part of https://github.com/dwrensha/animate-lean-proofs.
---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "Chess" where
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩,
+    ⟨`relaxedAutoImplicit, false⟩
+  ]
+
+@[default_target]
+lean_lib «Chess» where
+  -- add library configuration options here
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.11.0"
