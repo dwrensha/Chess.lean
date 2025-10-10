30f85ea91988a674475a06163db63a634ca4ff30
Moritz Firsching <firsching@google.com>
2025-10-10 23:52:35 +0200

    more helpful errors in tactics

---
+++ Chess/TacticsTest.lean
@@ -0,0 +1,55 @@
+import Chess.Tactics
+
+/-- error: It is Side.black's turn to move, try to use the `move` tactic instead of `opponent_move` -/
+#guard_msgs in
+theorem black_wins_back_rank :
+    ForcedWin .black
+      ╔════════════════╗
+      ║░░▓▓░░▓▓♜]▓▓♚}▓▓║
+      ║♟]░░▓▓░░▓▓♟]♟]♟]║
+      ║░░▓▓░░▓▓░░▓▓░░▓▓║
+      ║▓▓░░▓▓░░▓▓░░▓▓░░║
+      ║░░▓▓░░▓▓░░▓▓░░▓▓║
+      ║▓▓░░▓▓░░▓▓░░▓▓░░║
+      ║♙]♙]♙]▓▓░░♙]♙]♙]║
+      ║▓▓░░▓▓░░▓▓░░♔]░░║
+      ╚════════════════╝ := by
+  opponent_move
+  checkmate
+
+/-- error: It is Side.white's turn to move, try to use the `opponent_move` tactic instead of `move` -/
+#guard_msgs in
+/--
+Timman-Short 1990
+(from https://en.wikipedia.org/wiki/Smothered_mate)
+-/
+theorem smothered_mate :
+    ForcedWin .white
+      ╔════════════════╗
+      ║▓▓░░▓▓░░♜]░░▓▓♚]║
+      ║♟]▓▓♟]♖]♙]▓▓♟]♟]║
+      ║▓▓░░♟]░░▓▓░░▓▓░░║
+      ║░░▓▓░░▓▓░░♟]♘]▓▓║
+      ║▓▓░░♕]░░▓▓░░♞]░░║
+      ║♛]▓▓░░▓▓░░▓▓♙]▓▓║
+      ║♙]░░▓▓░░♙]♙]▓▓♙]║
+      ║░░▓▓░░▓▓░░▓▓♔}▓▓║
+      ╚════════════════╝ := by
+    move "Nf7"
+    move "Nh6"
+
+/-- error: It is Side.white's turn to move, try to use the `move` tactic instead to make a move that checkmates -/
+#guard_msgs in
+theorem white_wins_promotion_back_rank :
+    ForcedWin .white
+      ╔════════════════╗
+      ║░░▓▓░░▓▓░░▓▓♚]▓▓║
+      ║♟]░░♙]░░▓▓♟]♟]♟]║
+      ║░░▓▓░░▓▓░░▓▓░░▓▓║
+      ║▓▓░░▓▓░░▓▓░░▓▓░░║
+      ║░░▓▓░░▓▓░░▓▓░░▓▓║
+      ║▓▓░░▓▓░░▓▓░░▓▓░░║
+      ║♙]♙]░░▓▓░░♙]♙]♙]║
+      ║▓▓░░▓▓░░▓▓░░♔}░░║
+      ╚════════════════╝ := by
+  checkmate
